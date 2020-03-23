356c245f5a27f6f8a8217e7f5c7c6ab5b2c77b02
rangapv <rangapv@yahoo.com>
2020-03-23 14:03:37 +0000

    Kubelet restart script for cron

--- kubeletRestart.sh
+++ kubeletRestart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-OUTPUT=`ps -ef |grep kubelet|grep -v grep`
-cmd1="nohup sudo kubelet --kubeconfig $HOME/.kube/config --cluster-dns 10.96.0.10 & 2>&1 &"
+OUTPUT=`ps -ef |grep 'kubelet --kubeconfig'|grep -v grep`
+cmd1="cd /home/rangapv08/quikfix; nohup sudo kubelet --kubeconfig /home/rangapv08/.kube/config --cluster-dns 10.96.0.10 2>&1 &"
 if [ -z "$OUTPUT" ]
 then
   echo $(eval "$cmd1")
