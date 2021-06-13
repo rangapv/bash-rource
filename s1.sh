4a99f067ad676a221b2ddd85d42f5942fe91a7e5
rangapv <rangapv@yahoo.com>
2021-06-13 10:59:40 +0000

    This is the source file to find the distro of a Linux Box

---
+++ s1.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+set -E
+li=$(uname -s)
+
+if [ $(echo "$li" | grep Linux) ]
+then
+  mac=""
+else
+  mac=$(sw_vers | grep Mac)
+fi
+
+
+if [ -z "$mac" ]
+then
+  u1=$(cat /etc/*-release | grep ID= | grep ubuntu)
+  f1=$(cat /etc/*-release | grep ID= | grep fedora)
+  r1=$(cat /etc/*-release | grep ID= | grep rhel)
+  a1=$(cat /etc/*-release | grep ID= | grep amzn)
+  c1=$(cat /etc/*-release | grep ID= | grep centos)
+  s1=$(cat /etc/*-release | grep ID= | grep sles)
+  d1=$(cat /etc/*-release | grep ID= | grep debian)
+  fc1=$(cat /etc/*-release | grep ID= | grep flatcar)
+else 
+  echo "Mac is not empty"
+fi
+
+count=0
+
+if [ ! -z "$u1" ]
+then 
+	mi2="${mi,,}"
+	ji=$(cat /etc/*-release | grep DISTRIB_ID | awk '{split($0,a,"=");print a[2]}')
+	ki="${ji,,}"
+
+	if [ "$ki" = "ubuntu" ]
+	then
+   	echo "IT IS UBUNTU"
+   	cm1="apt-get"
+        cm11="add-apt-repository"
+   	cm2="apt-key"
+	count=1
+	fi
+elif [ ! -z "$d1" ]
+then
+	mi2="${mi,,}"
+	ji=$(cat /etc/*-release | grep ^ID= | grep -v "\"" | awk '{split($0,a,"=");print a[2]}')
+	ki="${ji,,}"
+	if [ "$ki" = "debian" ]
+	then
+	echo "IT IS Debian"
+	cm1="apt-get"
+	cm2="apt-key"
+	sudo $cm1 -y update
+	sudo $cm1 -y upgrade
+	sudo $cm1 -y install gcc make wget libffi-dev 
+        zlibadd
+	sslupdate $cm1 
+        count=1
+        fi
+
+elif [ ! -z "$f1" ]
+then
+	ji=$(cat /etc/*-release | grep '^ID=' |awk '{split($0,a,"\"");print a[2]}')
+        ki="${ji,,}"
+        if [ $ki = "fedora" ]
+        then
+        echo " it is fedora"
+        cm1="dnf"
+        sudo $cm1 -y install gcc make openssl-devel bzip2-devel libffi-devel zlib-devel wget
+	count=1
+        fi
+elif [ ! -z "$s1" ]
+then
+	ji=$(cat /etc/*-release | grep '^ID=' |awk '{split($0,a,"\"");print a[2]}')
+        ki="${ji,,}"
+        if [ $ki = "sles" ]
+        then
+        echo " it is SUSE"
+        sudo zypper install -y gcc make openssl-devel libffi-devel zlib-devel wget lsb-release
+	count=1
+        fi
+elif [ ! -z "$fc1" ]
+then
+	ji=$(cat /etc/*-release | grep '^ID=' |awk '{split($0,a,"\"");print a[2]}')
+        ki="${ji,,}"
+        if [ $ki = "flatcar" ]
+        then
+          echo "It is Flat Car Linux"
+        fi
+	count=0
+
+elif [[ ! -z "$c1" || ! -z "$r1" || ! -z "$a1" ]]
+then
+        ji=$(cat /etc/*-release | grep '^ID=' |awk '{split($0,a,"\"");print a[2]}')
+        ki="${ji,,}"
+	if [ $ki = "amzn" ]
+	then
+	   echo "It is amazon AMI"
+	   count=1
+	elif [ $ki = "rhel" ]
+	then 
+	   echo "It is RHEL"
+	   count=1
+	elif [ $ki = "centos" ]
+	then
+	   echo "It is centos"
+           count=1
+	else
+	   echo "OS flavor cant be determined"
+	fi
+elif [ ! -z "$mac" ]
+then
+	echo "It is a Mac"
+	cm1="brew"
+	count=1
+else
+	echo "The distribution cannot be determined"
+fi
